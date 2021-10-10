ae9eea596390c6f606f27e220209ad78050db886
lucas barra <lucastbarra@hotmail.com>
2021-10-10 20:38:26 -0300

    out of stock IU

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -3,7 +3,8 @@ import produce from 'immer';
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
-    items: []
+    items: [],
+    failedStockCheck: []
 }
 
 export const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
@@ -29,7 +30,7 @@ export const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
                 break;
             }
             case 'ADD_PRODUCT_TO_CART_FAILURE': {
-                console.log('faliure', action.payload)
+                draft.failedStockCheck.push(action.payload.productId);
 
                 break;
             }
